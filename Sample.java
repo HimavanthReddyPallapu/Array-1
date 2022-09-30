49d0df599a6a0151a71daf240eed7a5809cb7509
HimavanthReddyPallapu <113073979+HimavanthReddyPallapu@users.noreply.github.com>
2022-09-30 05:01:20 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,6 +1,41 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Three line explanation of solution in plain english
+// Time Complexity : N
+// Space Complexity : N
+// Did this code successfully run on Leetcode : YES
+// Three line explanation of solution in plain english 
 
-// Your code here along with comments explaining your approach
+// Your code here along with comments explaining your approach 
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        if(nums.length==0 || nums==null)
+        {
+            return nums;
+        }
+        
+        int[] a=new int[nums.length];
+        
+        
+        
+        a[0]=1;
+        
+        for(int i=1;i<nums.length;i++)
+        {
+            a[i]=a[i-1]*nums[i-1];
+        }
+        
+        int[] b=new int[nums.length];
+        b[nums.length-1]=1;
+        
+        for(int i=nums.length-2;i>=0;i--)
+        {
+            b[i]=b[i+1]*nums[i+1];
+        }
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            a[i]=a[i]*b[i];
+        }
+        
+        return a;
+    }
+}
